bb51c13f6b63e76b26a1c0e3947f2d66798dc260
Shanky <shashanksharma03.07@gmail.com>
2024-10-26 13:07:41 +0530

    navbar modified

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// function Navbar() {
-//   return (
-//     <div>
-//       Navbar {" "}
-//       <Link to={'/'}>
-//         Back-to-home
-//       </Link>
-//       {" "}
-//       <Link to={'/about-us'}>
-//         About-us
-//       </Link>
-//       {" "}
-//       <Link to={'/login'}>
-//         Login
-//       </Link>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
